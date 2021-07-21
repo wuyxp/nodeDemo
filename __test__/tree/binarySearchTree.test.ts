a234bcd7cdc30a4ef2663a036ff8067a5434d709
wuyang <wuyang@pensees.ai>
2021-07-21 15:49:44 +0800

    add: 增加二叉树的各种遍历方案，增加打印二叉树节点

--- __test__/tree/binarySearchTree.test.ts
+++ __test__/tree/binarySearchTree.test.ts
@@ -2,23 +2,63 @@ import Bst from '../../src/算法/树/binarySearchTree'
 
 
 describe('测试二叉平衡树', () => {
-  test("测试打印", () => {
-    const b = new Bst((node1: number, node2: number) => {
-      return node2 - node1
+  let b: Bst<number>
+  beforeEach(() => {
+    b = new Bst((node1, node2) => {
+      return node1 - node2
     })
     b.add(10)
     b.add(8)
     b.add(12)
-    // b.add(4)
-    // b.add(19)
-    // b.add(9)
-    // b.add(11)
-    // b.add(7)
-    // b.add(16)
-    // b.add(2)
-    // b.add(5)
+    b.add(4)
+    b.add(19)
+    b.add(9)
+    b.add(11)
+    b.add(7)
+    b.add(16)
+    b.add(2)
+    b.add(5)
+  })
 
+  test("测试打印", () => {
     b.print()
-    // expect(r).toBe(7)
+  })
+  test("前序遍历", () => {
+    const result:number[] = []
+    const exp = [10, 8, 4, 2, 7, 5, 9, 12, 11, 19, 16]
+    b.preorder(node => {      
+      result.push(node)
+    })
+    expect(result).toEqual(exp)
+  })
+  test("中序遍历", () => {
+    const result:number[] = []
+    const exp = [2, 4, 5, 7, 8, 9, 10, 11, 12, 16, 19]
+    b.inorder(node => {      
+      result.push(node)
+    })
+    expect(result).toEqual(exp)
+  })
+  test("后序遍历", () => {
+    const result:number[] = []
+    const exp = [2, 5, 7, 4, 9, 8, 11, 16, 19, 12, 10]
+    b.postorder(node => {      
+      result.push(node)
+    })
+    expect(result).toEqual(exp)
+  })
+
+  test("层序遍历", () => {
+    const result:number[] = []
+    const exp = [10, 8, 12, 4, 9, 11, 19, 2, 7, 16, 5]
+    b.levelorder(node => {      
+      result.push(node)
+    })
+    expect(result).toEqual(exp)
+  })
+
+  test("获取行高", () => {
+    const h = b.levelorder()
+    expect(h).toEqual(5)
   })
 })
--- src/算法/树/binarySearchTree.ts
+++ src/算法/树/binarySearchTree.ts
@@ -3,6 +3,12 @@
  * 又叫做 二叉排序树 二叉查找树
  * 任意一个节点的值都大于其左子树所有节点的值
  * 任意一个节点的值都小于其右子树所有节点的值
+ * 
+ * 前序遍历
+ * 中序遍历
+ * 后续遍历
+ * 层级遍历
+ * 计算高度
  */
 
 class Node<E> {
@@ -16,12 +22,15 @@ class Node<E> {
   }
 }
 
+type compareFun<E> = (node1: E, node2: E) => number;
+type orderFun<E> = (element: E) => void
+
 export default class BinarySearchTree<E> {
 
   private _size = 0;
   public root: Node<E> | null = null
-  private compare: (node1: E, node2: E) => number;
-  constructor(compare: (node1: E, node2: E) => number) {
+  private compare: compareFun<E>
+  constructor(compare: compareFun<E>) {
     if (compare === undefined || typeof compare !== 'function') {
       this.compare = (n1: E, n2: E) => (n1 as unknown as number) - (n2 as unknown as number)
     } else {
@@ -63,6 +72,7 @@ export default class BinarySearchTree<E> {
           node = node.right
         } else {
           // 比较相等
+          node.element = element
           return
         }
       }
@@ -84,8 +94,120 @@ export default class BinarySearchTree<E> {
     return false
   }
 
+  /**
+   * 前序遍历： 先遍历根节点，然后依次遍历 左子树，右子树
+   * @param callback 回调
+   */
+  public preorder (callback: orderFun<E>) {
+    if (this.root === null) return
+    this._preorder(this.root, callback)
+  }
+
+  // 递归方式前序遍历
+  private _preorder(node: Node<E> | null, callback: orderFun<E>) {
+    if (node === null) return
+    typeof callback === 'function' && callback(node.element)
+    this._preorder(node.left, callback)
+    this._preorder(node.right, callback)
+  }
+
+  /**
+   * 中序遍历
+   * @param callback 回调
+   */
+  public inorder(callback: orderFun<E>) {
+    if (this.root === null) return
+    this._inorder(this.root, callback)
+  }
+
+  // 递归方式中序遍历
+  private _inorder(node: Node<E> | null, callback: orderFun<E>) {
+    if (node === null) return
+    this._inorder(node.left, callback)
+    typeof callback === 'function' && callback(node.element)
+    this._inorder(node.right, callback)
+  }
+  
+
+  /**
+   * 后续遍历
+   * @param callback 回调
+   */
+  public postorder(callback: orderFun<E>) {
+    if (this.root === null) return
+    this._postorder(this.root, callback)
+  }
+
+  // 递归方式后序遍历
+  private _postorder(node: Node<E> | null, callback: orderFun<E>) {
+    if (node === null) return
+    this._postorder(node.left, callback)
+    this._postorder(node.right, callback)
+    typeof callback === 'function' && callback(node.element)
+  }
+  
+
+  /**
+   * 层级遍历并且获取高度:
+   * 1. 使用数组来模拟队列，只是用 入队：push，出队：shift，队长度：length
+   * 2. 首先入队 root 节点，并且高度 h 设为 1，设当前 行大小 s 为 1，下一行 n = 0
+   * 3. 对头出队元素，并且将该元素的 左节点，右节点 分别进行入队操作，进行 n++,s--
+   * 4. 当 s = 0 说明当前行完成，h+1，下一行的大小赋值给 s，下一行重置为n = 0
+   * 5. 当队列为空时，返回 高度
+   * @param callback 回调
+   * @returns 返回行高
+   */
+  public levelorder(callback?: orderFun<E>): number {
+    const queue: Node<E>[] = []
+    let h = 0 // 行高
+    let s = 0 // 当前行的大小 
+    let n = 0 // 下一行的大小 
+
+    if (this.root === null) return 0
+    queue.push(this.root)
+    s = 1
+
+    while(queue.length > 0) {
+      const node: Node<E> = queue.shift() as Node<E>
+      typeof callback === 'function' && callback(node.element)
+      s--;
+      
+      if (node.left !== null) {
+        queue.push(node.left)
+        n++
+      }
+      if (node.right !== null) {
+        queue.push(node.right)
+        n++
+      }
+      if (s === 0) {
+        // 当前行完成
+        h++
+        s = n
+        n = 0
+      }
+    }
+
+    return h
+  }
+
+  private _printOrder(node:Node<E> | null, pre: string, result: string): string {
+    if (node === null) return result
+    result = result + pre + node.element + '\n'
+    result = this._printOrder(node.left, pre + 'l--', result)
+    result = this._printOrder(node.right, pre + 'r--', result)
+    return result
+  }
+
   public print(): void {
-    console.log(this.root)
-    console.log(this.size())
+    if (this.root !== null) {
+      let r = '总节点为: ' + this._size + '\n'
+      // console.log(this._printOrder(this.root, '-', ''));
+      
+      r = r + this._printOrder(this.root, '', '')
+      console.log(r);
+    } else {
+      console.log('节点为空');
+    }
   }
 }
